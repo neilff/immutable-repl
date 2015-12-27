d9a1fe7cf2bb12cb6b49feae8a402f9b53117d4e
Neil Fenton <neilff@gmail.com>
2015-12-27 13:33:13 -0500

    added improvements: split view, localstorage saves, smaller ui for usability on small screens

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,36 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { fromJS } from 'immutable';
 
 import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 import logger from './logger';
 import queryMiddleware from '../middleware/queryMiddleware';
 import rootReducer from '../reducers';
 
+const storageConfig = {
+  key: 'immutable-repl',
+  serialize: (store) => {
+    return store && store.ui ?
+      JSON.stringify(store.ui.toJS()) : store;
+  },
+  deserialize: (state) => ({
+    ui: state ? fromJS(JSON.parse(state)) : fromJS({}),
+  }),
+};
+
 function configureStore(initialState) {
   const store = compose(
-    applyMiddleware(
-      thunk,
-      queryMiddleware,
-      logger,
-    ),
+    __DEV__ ?
+      applyMiddleware(
+        thunk,
+        queryMiddleware,
+        logger,
+      ) :
+      applyMiddleware(
+        thunk,
+        queryMiddleware,
+      ),
+    persistState('ui', storageConfig),
   )(createStore)(rootReducer, initialState);
 
   if (module.hot) {
